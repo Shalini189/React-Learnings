b397b9f09b311fd06e65328c08a057443222e98f
Shalini189 <shalinicg.027@gmail.com>
2024-10-03 00:39:25 +0530

    new project

--- App.js
+++ App.js
@@ -1,8 +1,4 @@
-const heading = React.createElement("div",{id:"parent", xyz:"abc"},["my first react program",
-React.createElement("div",{id:"child"}, React.createElement("h1",{},"h1 tag")),
-React.createElement("div",{id:"child"}, React.createElement("h1",{},"h1 tag"))
-]);
-const root = ReactDOM.createRoot(document.getElementById("container"));
-const para = ReactDOM.createRoot(document.getElementById("par"));
-root.render(heading);
-para.render("jjs")
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
